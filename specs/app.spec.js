457f9252ed1cfc0edb4465163153a37336804d9e
Bruno Macedo <contato@brunomacedo.com.br>
2018-06-14 10:49:25 -0300

    passing test

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,13 +1,13 @@
 describe('Jenkins Deploy Tests', () => {
-  it('Should run test 1', (done) => {
+  it('Should run test 1', () => {
     console.log('1 test...');
   });
 
-  it('Should run test 2', (done) => {
+  it('Should run test 2', () => {
     console.log('2 test...');
   });
 
-  it('Should run test 3', done => {
+  it('Should run test 3', () => {
     console.log('3 test...');
   });
 });
