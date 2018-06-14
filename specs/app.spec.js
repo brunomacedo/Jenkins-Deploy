33c37d6f4c4a1249aae232145899889480d0f4e1
Bruno Macedo <contato@brunomacedo.com.br>
2018-06-14 10:45:09 -0300

    Run tests spec test

---
+++ specs/app.spec.js
@@ -0,0 +1,13 @@
+describe('Jenkins Deploy Tests', () => {
+  it('Should run test 1', (done) => {
+    console.log('1 test...');
+  });
+
+  it('Should run test 2', (done) => {
+    console.log('2 test...');
+  });
+
+  it('Should run test 3', done => {
+    console.log('3 test...');
+  });
+});
